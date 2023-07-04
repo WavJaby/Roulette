d8a4dbf3902db5518cd2d70ee80a6ccaa4cf3265
WavJaby <wavjaby@gmail.com>
2023-07-05 00:51:52 +0800

    make it stop slowly

--- index.js
+++ index.js
@@ -16,37 +16,52 @@ window.addEventListener('load', function () {
     arrow.classList.add('arrow');
     container.appendChild(arrow);
 
+    const standard = gaussian(0, 10);
+    let a = 0;
+    for (let i = 0; i < 1000000000; i++) {
+        const j = standard();
+        if (j > a)
+            a = j;
+    }
+    console.log(a)
 
-    let startTime = 0;
+    // 獎金池
+    let spinning = false;
+    let entranceFee = 10000;
+    let bonusPoolValue = 0;
+    const bonusPool = document.createElement('h1');
+    updateBonusText();
+    document.body.appendChild(bonusPool);
+
     window.addEventListener('keyup', function (ev) {
         if (ev.key === ' ') {
+            if (spinning)
+                return;
+            spinning = true;
+            bonusPoolValue += entranceFee;
+            updateBonusText();
             console.log('start');
 
-            startSpin(roulette, -90 - (140 + Math.random() * 220));
+            startSpin(roulette, -90 - (140 + Math.random() * 220), function () {
+                spinning = false;
+            });
         }
+    });
 
-        // if (ev.key === 'w') {
-        //     const timePass = window.performance.now() - startTime;
-        //     let percent = timePass / duration;
-        //     if (percent > 1) percent = 1;
-        //     console.log(angle * cubicBezier(percent, {x: 0.3, y: 0.8}, {x: 0.4, y: 1}));
-        // }
-    })
+    function updateBonusText() {
+        bonusPool.innerText = '獎金池 : ' + format(bonusPoolValue) + ' 元';
+    }
 });
 
-function startSpin(obj, angle) {
+function startSpin(obj, angle, onStop) {
     if (!obj.angle)
         obj.angle = 0;
 
-    const startMaxTurnAngle = 360 * 2;
-    const endTurnAngle = 360 * 5;
+    const startMaxTurnAngle = 360 * 5;
+    const endTurnAngle = 360 * 4;
 
-    const bezier = [0, 0, 1, 1];
+    // const bezier = [0, 0, 1, 1];
     const speed = 360 * 3; // Deg per sec
-
-    for (let i = 0; i < 1; i += 0.1) {
-        console.log(cubicBezier(i, bezier[0], bezier[1], bezier[2], bezier[3]));
-    }
 
     const startTurnAngle = startMaxTurnAngle - obj.angle;
     const startDuration = startTurnAngle / speed * 1000;
@@ -71,30 +86,41 @@ function startSpin(obj, angle) {
             stopSpin();
     }
 
+    const stopSpeed = speed;
+    let stopTotalAngle;
     let stopDuration;
 
     function stopSpin() {
         startAngle = obj.angle;
-        angle += endTurnAngle - obj.angle;
-        const startSpeed = (cubicBezier(0.01, bezier[0], bezier[1], bezier[2], bezier[3]) - cubicBezier(0, bezier[0], bezier[1], bezier[2], bezier[3])) / 0.01;
+        stopTotalAngle = angle += endTurnAngle - obj.angle;
+        // const startSpeed = (cubicBezier(0.01, bezier[0], bezier[1], bezier[2], bezier[3]) - cubicBezier(0, bezier[0], bezier[1], bezier[2], bezier[3])) / 0.01;
 
-        stopDuration = angle * startSpeed / speed * 1000;
+        // stopDuration = angle * startSpeed / speed * 1000;
         startTime = window.performance.now();
         requestAnimationFrame(stop);
     }
 
     function stop() {
         const time = (window.performance.now() - startTime);
-        let newTime = time / stopDuration;
-        if (newTime > 1)
-            newTime = 1;
-        newTime = cubicBezier(newTime, bezier[0], bezier[1], bezier[2], bezier[3]);
-        const nowAngle = (startAngle + angle * newTime) % 360;
-        obj.angle = nowAngle;
-        obj.style.rotate = nowAngle + 'deg';
+        const m = Math.min(1, (angle + 5) / stopTotalAngle);
+        let angleChange = stopSpeed * m / 1000 * time;
 
-        if (time < stopDuration)
+        console.log(angleChange)
+
+        if (angleChange > angle)
+            angleChange = angle;
+
+        angle -= angleChange;
+        const nowAngle = (obj.angle + angleChange) % 360;
+        obj.angle = nowAngle;
+        obj.style.rotate = nowAngle + 'deg'
+
+        if (angle > 0)
             requestAnimationFrame(stop);
+        else
+            onStop();
+
+        startTime = window.performance.now();
     }
 }
 
@@ -205,6 +231,11 @@ function createSvgText(x, y, text, size, color, options) {
     return element;
 }
 
+function format(number) {
+    return String(number)
+        .replace(/(.)(?=(\d{3})+$)/g, '$1,');
+}
+
 function bezier(t, p0, p1, p2, p3) {
     const cX = 3 * (p1.x - p0.x),
         bX = 3 * (p2.x - p1.x) - cX,
@@ -227,20 +258,37 @@ function cubicBezier(t, x1, y1, x2, y2) {
     //     Math.pow(t, 3) * P4;
 
     const p1y = 0, p4y = 1;
-    return  Math.pow(1 - t, 3) * p1y +
+    return Math.pow(1 - t, 3) * p1y +
         3 * Math.pow(1 - t, 2) * t * y1 +
         3 * (1 - t) * Math.pow(t, 2) * y2 +
         Math.pow(t, 3) * p4y;
 }
 
-// function bezier(t, p1, p2) {
-//     const initial = 0;
-//     const final = 1;
-//     return (1 - t) * (1 - t) * (1 - t) * initial
-//         +
-//         3 * (1 - t) * (1 - t) * t * p1
-//         +
-//         3 * (1 - t) * t * t * p2
-//         +
-//         t * t * t * final;
-// }
+// returns a gaussian random function with the given mean and stdev.
+function gaussian(mean, stdev) {
+    let y2;
+    let use_last = false;
+    return function () {
+        let y1;
+        if (use_last) {
+            y1 = y2;
+            use_last = false;
+        } else {
+            let x1, x2, w;
+            do {
+                x1 = 2.0 * Math.random() - 1.0;
+                x2 = 2.0 * Math.random() - 1.0;
+                w = x1 * x1 + x2 * x2;
+            } while (w >= 1.0);
+            w = Math.sqrt((-2.0 * Math.log(w)) / w);
+            y1 = x1 * w;
+            y2 = x2 * w;
+            use_last = true;
+        }
+
+        const retVal = mean + stdev * y1;
+        if (retVal > 0)
+            return retVal;
+        return -retVal;
+    }
+}
